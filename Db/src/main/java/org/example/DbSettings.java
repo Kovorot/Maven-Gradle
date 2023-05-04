deccc444d613e1d503a9768b30878b487646e79d
Andrey <kovorot0875@mail.ru>
2023-05-04 07:57:08 +0300

    Gradle added

---
+++ Db/src/main/java/org/example/DbSettings.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DbSettings {
+    private  String name;
+    private String password;
+
+    public DbSettings (String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+
+}
